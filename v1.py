3ed0aa3f74db0dcc963ec2be02a61b59d78bda3b
espeon-lab <jd390238@gmail.com>
2024-12-19 08:19:01 -0500

    Create v1.py

---
+++ v1.py
@@ -0,0 +1,9 @@
+price = float(input("Enter the price of the item: "))
+
+sales_tax = price * 0.06625
+
+total_cost = price + sales_tax
+
+print("Sales tax amount:", sales_tax)
+
+print("Total cost:", total_cost)
